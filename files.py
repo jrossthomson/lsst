6b70ea27090d8767286685cbcc9a19e243c04a12
Ross Thomson (drj) <jrossthomson@google.com>
2018-09-11 10:34:58 -0400

    Cleaned up and added FLAGS

--- files.py
+++ files.py
@@ -1,11 +1,75 @@
-import urllib
 import bz2
+import re
+import requests
+import shutil
+import urllib
+from google.cloud import storage
+from google.cloud import bigquery
 
-def url2local(src_url, dest_file):
-  f = urllib.URLopener()
-  f.retrieve(src_url, dest_file)
+
+def download_file(src_url, dest_file):
+  r = requests.get(src_url, stream=True)
+  with open(dest_file, 'wb') as f:
+    shutil.copyfileobj(r.raw, f)
+  print('Downloaded {} to {}.'.format(src_url, dest_file))
 
 def bunzip2(src_file, dest_file):
+  with open(src_file, 'rb') as source, open(dest_file, 'wb') as dest:
+    dest.write(bz2.decompress(source.read()))
+  print('File {} bunziped to {}.'.format(src_file, dest_file))
+
+def bunzip2_mem(src_file, dest_file):
+  print src_file
+  print dest_file
   zipfile = bz2.BZ2File(src_file) # open the file
   data = zipfile.read() # get the decompressed data
   open(dest_file, 'wb').write(data) # write a uncompressed file
+  print('File {} bunziped to {}.'.format(src_file, dest_file))
+
+def load2bq(uri='', 
+        dataset_id='LSST', 
+        table_id='mep_wise',
+        delimiter='|'):
+  client = bigquery.Client()
+
+  dataset_ref = client.dataset(dataset_id)
+  table_ref = dataset_ref.table(table_id)
+  table = client.get_table(table_ref)  # API Request
+
+  dataset_ref = client.dataset(dataset_id)
+  job_config = bigquery.LoadJobConfig()
+  job_config.schema = table.schema
+  job_config.source_format = bigquery.SourceFormat.CSV
+  job_config.field_delimiter = delimiter
+  job_config.create_disposition = 'CREATE_IF_NEEDED'
+
+  load_job = client.load_table_from_uri(uri, table_ref, job_config=job_config)  # API request
+
+  print('Starting job {}'.format(load_job.job_id))
+  load_job.result()  # Waits for table load to complete.
+  print('Job finished.')
+
+  print('Loaded table now has {} rows.'.format(table.num_rows))
+
+def gcs_remove(bucket_name, blob_name):
+  """Uploads a file to the bucket."""
+  storage_client = storage.Client()
+  bucket = storage_client.get_bucket(bucket_name)
+  blob = bucket.blob(blob_name)
+  blob.delete()
+  print('File {} removed from GCS.'.format(blob_name))
+
+def upload2gcs(bucket_name, src_file, dest_blob):
+  """Uploads a file to the bucket."""
+  storage_client = storage.Client()
+  bucket = storage_client.get_bucket(bucket_name)
+  blob = bucket.blob(dest_blob)
+  blob.upload_from_filename(src_file)
+  print('File {} uploaded to GCS: {}.'.format(src_file, dest_blob))
+
+def gethttpfilesize(url):
+    print "opening url:", url
+    site = urllib.urlopen(url)
+    meta = site.info()
+    print "Content-Length:", meta.getheaders("Content-Length")[0]
+
--- task.py
+++ task.py
@@ -1,5 +1,54 @@
 import files as files
+import sys
+import os
+from urlparse import urlparse
+
+from absl import app
+from absl import flags
+
+FLAGS = flags.FLAGS
+
+flags.DEFINE_string('uri', '', 'URI from whence to download the data.')
+flags.DEFINE_bool('download', True, 'Download from web')
+flags.DEFINE_bool('bunzip2', True, 'Bunzip the files')
+flags.DEFINE_bool('gcs_upload', True, 'Upload to Google Cloud Storage')
+flags.DEFINE_bool('bq_load', True, 'Load to Google Cloud BigQuery Table')
+flags.DEFINE_bool('cleanup', True, 'If true, removes files')
+flags.DEFINE_string('bucket', 'lsst-mep', 'Name of Google Cloud Storage Bucket')
+flags.DEFINE_string('dataset_id', 'LSST', 'Name of Google Cloud BigQuery Dataset')
+flags.DEFINE_string('table_id', 'mep-wise', 'Name of Google Cloud BigQuery Table')
 
 
-files.url2local('https://storage.googleapis.com/lsst-wise/chick.png.bz2', 'chick.png.bz2')
-files.bunzip2('chick.png.bz2', 'c.png')
+def main(argv):
+  # Parse the URL
+  the_url = FLAGS.uri
+  parsed_url = urlparse(the_url)
+  the_path = parsed_url.path
+  raw_filename = os.path.split(the_path)[-1]
+  filename, file_extension = os.path.splitext(raw_filename)
+
+  # Do the work if the flags allow
+
+  if FLAGS.download: files.download_file(the_url, raw_filename)
+  if FLAGS.bunzip2: files.bunzip2(raw_filename, filename)
+  if FLAGS.gcs_upload: files.upload2gcs(FLAGS.bucket, filename, filename)
+  if FLAGS.bq_load: files.load2bq(uri='gs://' + FLAGS.bucket + '/' + filename,
+      dataset_id='LSST',
+      table_id='mep_wise')
+
+  if FLAGS.cleanup: 
+    try:
+      os.remove(filename)
+    except:
+      pass
+    try:
+     os.remove(raw_filename)
+    except:
+      pass
+    try:
+     files.gcs_remove(FLAGS.bucket, filename)
+    except:
+      pass
+
+if __name__ == '__main__':
+  app.run(main)
